35c0e4b127568c4f59617b3ce342ae6910baac6c
TemporarYLife <aay23@tpu.ru>
2021-10-27 15:33:29 +0700

    Done HW1

---
+++ Task1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//-----------------------ATTENTION--------------- Задание 2 - сложность функции равна   O(N^3+2)
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите N: ");
+            int n = int.Parse(Console.ReadLine());
+            int d = 0, i = 2;
+            while (i<n)
+            {
+                if (n % i == 0)
+                {
+                    d++;
+                }
+                i++;
+
+            }
+
+            if (d == 0)
+            {
+                Console.WriteLine("Простое");
+            }
+            else
+            {
+                Console.WriteLine("Не простое");
+            }    
+        
+        }
+    }
+}
