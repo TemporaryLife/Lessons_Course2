e80b04d352ede42b2810ead1ea7a51c46f920e3e
aay23 <aay23@tpu.ru>
2021-11-11 17:02:57 +0700

    done

---
+++ HW2_Task1/HW2_Task1/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace HW2_Task1
+{
+    public class DLinkedList : ILinkedList
+    {
+        public Node First;
+        public Node Last;
+        public int count;
+        public void AddNode(int value)
+        {
+
+            var AddedNode = new Node { Value = value };
+            if (First==null)
+            {
+                First = AddedNode;
+            }
+            else
+            {
+                Last.NextNode = AddedNode;
+                AddedNode.PrevNode = Last;
+            }
+            Last = AddedNode;
+            count++;
+        }
+
+
+        public void AddNodeAfter(Node node, int value)
+        {
+            var Added_Node = new Node { Value = value };
+            if (First==null)
+            {
+                First = Added_Node;
+            }
+
+            else
+            {
+                node.NextNode.PrevNode = Added_Node;
+                node.NextNode = Added_Node;
+            }
+        }
+
+        public Node FindNode(int searchValue)
+        {
+            var current = new Node();
+            current = First;
+            while(current!=null)
+            {
+                if (current.Value==searchValue)
+                {
+                    return current;
+                }
+                current = current.NextNode;
+            }
+            return null;
+        }
+
+        public int GetCount()
+        {
+            return count;
+        }
+
+        public void RemoveNode(int index)
+        {
+            int list_index = 0;
+            Node current = First;
+
+            while (list_index!=index)
+            {
+                if (current.NextNode!=null)
+                {
+                    current = current.NextNode;
+                }
+                else
+                {
+                    break;
+                    Console.WriteLine("За пределами массива");
+                }
+                list_index++;
+            }
+            if (current==Last)
+            {
+                Last = current.PrevNode;
+            }
+            if (current==First)
+            {
+                First = current.NextNode;
+            }
+            current.PrevNode.NextNode = current.NextNode;
+
+        }
+
+        public void RemoveNode(Node node)
+        {
+            Node current = First;
+
+            while (current != null)
+            {
+                if (current.Value==node.Value)
+                {
+                    break;
+                }
+                current = current.NextNode;
+            }
+            if (current!=null)
+            {
+                if (current.NextNode!=null)
+                {
+                    current.NextNode.PrevNode = current.PrevNode;
+                }
+                else
+                {
+                    Last = current.PrevNode;
+                }
+            }
+            if (current.PrevNode!=null)
+            {
+                current.PrevNode.NextNode = current.NextNode;
+            }
+            else
+            {
+                First = current.NextNode;
+            }
+            count--;
+        }
+    }
+
+    public class Node
+    {
+        public int Value { get; set; }
+        public Node NextNode { get; set; }
+        public Node PrevNode { get; set; }
+
+    }
+    public interface ILinkedList
+    {
+        int GetCount(); // возвращает количество элементов в списке
+        void AddNode(int value);  // добавляет новый элемент списка
+        void AddNodeAfter(Node node, int value); // добавляет новый элемент списка после определённого элемента
+        void RemoveNode(int index); // удаляет элемент по порядковому номеру
+        void RemoveNode(Node node); // удаляет указанный элемент
+        Node FindNode(int searchValue); // ищет элемент по его значению
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ HW2_Task1/HW2_Task2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace HW2_Task2
+{
+    class Program
+    {
+        public class TestCase
+        {
+            public int[] inputArray { get; set; }
+            public int searchValue { get; set; }
+            public int Expected { get; set; }
+            public Exception ExpectedException { get; set; }
+        }
+
+        public static int BinarySearch(int[] inputArray, int searchValue)
+        {
+            if (searchValue<=0)
+            {
+                throw new ArgumentException("Searching Value should be positive");
+            }
+
+            int min = 0;
+            int max = inputArray.Length - 1;
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (searchValue == inputArray[mid])
+                {
+                    return mid;
+                }
+                else if (searchValue < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+        static void Test(TestCase testCase)
+        {
+            try
+            {
+                var actual = BinarySearch(testCase.inputArray, testCase.searchValue);
+                if (actual==testCase.Expected)
+                {
+                    Console.WriteLine("VALID TEST");
+
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+
+            }
+            catch (Exception e)
+            {
+                if (testCase.ExpectedException != null && e.GetType() == testCase.ExpectedException.GetType())
+                {
+                    Console.WriteLine("Valid Test");
+                }
+                else
+                {
+                    Console.WriteLine("INVALID TEST");
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var Test1 = new TestCase
+            {
+                inputArray = new int[] { 1, 3, 5, 7, 5, 9, 10 },
+                searchValue = 5,
+                Expected = 2,
+                ExpectedException = null
+
+            };
+            var Test2 = new TestCase
+            {
+                inputArray = new int[] { 1, 3, 5, 7, 5, 9, 10 },
+                searchValue = 11,
+                Expected = -1,
+                ExpectedException = null
+
+            };
+            var Test3 = new TestCase
+            {
+                inputArray = new int[] { 1, 3, 5, 7, 5, 9, 10 },
+                searchValue = 3,
+                Expected = 5,
+                ExpectedException = null
+
+            };
+            var Test4 = new TestCase
+            {
+                inputArray = new int[] { 1, 3, 5, 7, 5, 9, 10 },
+                searchValue = -1,
+                Expected = -1,
+                ExpectedException = new ArgumentException()
+
+            };
+
+            Test(Test1);
+            Test(Test2);
+            Test(Test3);
+            Test(Test4);
+        }
+    }
+}
